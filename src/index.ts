78b36b1bc939998c5f6f4ce21dbf9f3cf1627ea6
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-12 23:55:53 +0700

    Complete the basic route

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 // routes
+import { TeacherRouter } from "./routes/teacherRoute";
+import { StudentRouter } from "./routes/StudentRoute";
 
 // config
 dotenv.config();
@@ -15,6 +17,10 @@ const key = process.env.MONGO_KEY;
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use("/teacher", TeacherRouter);
+app.use("/student", StudentRouter);
 
 // connect to mongodb
 mongoose.connect(
