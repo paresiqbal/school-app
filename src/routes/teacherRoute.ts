a5ff7da74ac49524fc8deb1cc636249a6f8fd38e
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-12 23:32:18 +0700

    Create teacher route

---
+++ src/routes/teacherRoute.ts
@@ -0,0 +1,10 @@
+// libary
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+// register account
+router.post("/register", (req: Request, res: Response) => {});
+
+// login account
+router.post("/login", (req: Request, res: Response) => {});
