1dfac396cd8ca2d8d50058b7fc6ecb15e2d2d8a0
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-13 23:24:20 +0700

    asdasdas

--- src/routes/studentRoute.ts
+++ src/routes/studentRoute.ts
@@ -4,7 +4,9 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 // register account
-router.post("/register", (req: Request, res: Response) => {});
+router.post("/register", (req: Request, res: Response) => {
+  res.send("register");
+});
 
 // login account
 router.post("/login", (req: Request, res: Response) => {});
