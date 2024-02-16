07bb2a0fa06210a791deb7c049ffa6cd68d1ef53
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-16 20:03:57 +0700

    Add Admin schema

---
+++ src/models/Admin.ts
@@ -0,0 +1,17 @@
+// library
+import { Schema, model } from "mongoose";
+
+interface IAdmin {
+  _id?: string;
+  username: string;
+  password: string;
+  role: string;
+}
+
+const AdminSchema = new Schema<IAdmin>({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+});
+
+export default model<IAdmin>("Admin", AdminSchema);
