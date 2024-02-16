a25b7dc2ab0a6e84975528e41dfea4531ed0cfeb
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-17 01:01:08 +0700

    Modified if checking

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,8 +4,11 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 // models
-import Student from "../models/Student";
-import Teacher from "../models/Teacher";
+import { StudentModel } from "../models/Student";
+import { TeacherModel } from "../models/Teacher";
+
+// error handling
+import { UserErrors } from "../enumError";
 
 const router = Router();
 
@@ -14,7 +17,16 @@ router.post("/register", async (req: Request, res: Response) => {
   const { username, password, role } = req.body;
 
   try {
-    const findStudents = await Student.findOne;
+    // check if username already exists
+    const findStudents = await StudentModel.findOne({ username });
+    const findTeachers = await TeacherModel.findOne({ username });
+    if (findStudents || findTeachers) {
+      return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ username, password: hashedPassword });
   } catch (error) {}
 });
 
