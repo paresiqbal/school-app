1cb6003e749db3f2478019f1b88a37b7e382cc92
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-23 13:53:43 +0700

    Update Teacher models

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -5,12 +5,16 @@ export interface ITeacher {
   _id?: string;
   username: string;
   password: string;
+  fullname: string;
+  nip: string;
   role: string;
 }
 
 const TeacherSchema = new Schema<ITeacher>({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  fullname: { type: String, required: true },
+  nip: { type: String, required: true },
   role: { type: String, default: "teacher" },
 });
 
