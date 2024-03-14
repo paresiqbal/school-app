d95c452aac705848e70c5812eabaa106d946e240
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-14 14:31:49 +0700

    Commented out teacher field in Attendance model

--- src/models/Attendance.ts
+++ src/models/Attendance.ts
@@ -4,21 +4,15 @@ export interface IAttendance {
   _id?: string;
   date: Date;
   student: Types.ObjectId;
-  teacher: Types.ObjectId;
+  // teacher: Types.ObjectId;
   class: Types.ObjectId;
-  status: "present" | "sick" | "absent";
 }
 
 const AttendanceSchema = new Schema<IAttendance>({
   date: { type: Date, required: true },
-  teacher: { type: Schema.Types.ObjectId, ref: "Teacher", required: true },
-  student: { type: Schema.Types.ObjectId, ref: "Student", required: true },
   class: { type: Schema.Types.ObjectId, ref: "Class", required: true },
-  status: {
-    type: String,
-    enum: ["present", "sick", "absent"],
-    required: true,
-  },
+  // teacher: { type: Schema.Types.ObjectId, ref: "Teacher", required: true },
+  student: { type: Schema.Types.ObjectId, ref: "Student", required: true },
 });
 
 export const AttendanceModel = model<IAttendance>(
