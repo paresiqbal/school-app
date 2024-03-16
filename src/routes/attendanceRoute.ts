3e37106c2a4f05e25e3ee574d4c5c5bd5df45804
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-16 13:10:25 +0700

    Refactor attendanceRoute.ts to update attendance status for multiple students

--- src/routes/attendanceRoute.ts
+++ src/routes/attendanceRoute.ts
@@ -1,38 +1,48 @@
+// library
 import { Router, Request, Response } from "express";
+
+// models
 import { TeacherModel } from "../models/Teacher";
 import { AttendanceStatus, StudentModel } from "../models/Student";
 
 const router = Router();
 
 router.post("/check-attendance", async (req: Request, res: Response) => {
-  const { teacherId, studentId, classId, date } = req.body;
+  const { teacherId, classId, date, studentIds } = req.body;
 
   try {
+    // Find & check teacher
     const teacher = await TeacherModel.findById(teacherId);
     if (!teacher) {
       return res.status(404).json({ error: "Teacher not found" });
     }
 
-    const student = await StudentModel.findById(studentId);
-    if (!student) {
-      return res.status(404).json({ error: "Student not found" });
+    // Find & check all students in the class
+    const students = await StudentModel.find({ class: classId });
+    if (!students.length) {
+      return res.status(404).json({ error: "No students found in this class" });
     }
 
+    await StudentModel.updateOne(
+      { _id: { $in: studentIds }, class: classId }, // Filter by student IDs and class
+      { attendanceStatus: AttendanceStatus.Present }
+    );
+
+    // Generate attendance report format
     const attendanceReport = {
       "attendance-class": classId,
       date,
       teacher: teacher.fullname,
-      "students-record": [
-        {
-          id: student._id,
-          fullname: student.fullname,
-          class: classId,
-          status: student.attendanceStatus || AttendanceStatus.Absent,
-        },
-      ],
+      "students-record": students.map((student) => ({
+        id: student._id,
+        fullname: student.fullname,
+        class: classId,
+        status: student.attendanceStatus || AttendanceStatus.Absent,
+      })),
     };
 
     res.json(attendanceReport);
+    res.json({ message: "Attendance status updated successfully" });
   } catch (error) {
     console.error("Error while checking attendance:", error);
     res.status(500).json({ error: "Internal server error" });
