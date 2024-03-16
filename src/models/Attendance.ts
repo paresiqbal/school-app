e55af6b80198a87bd55f5a612e62ff7eb607aeb6
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-16 16:46:18 +0700

    Update attendance models

--- src/models/Attendance.ts
+++ src/models/Attendance.ts
@@ -1,18 +1,27 @@
 import { Schema, model, Types } from "mongoose";
 
-export interface IAttendance {
+interface IAttendance {
   _id?: string;
   date: Date;
   student: Types.ObjectId;
   teacher: Types.ObjectId;
   class: Types.ObjectId;
+  isPresent: "present" | "absent" | "excused";
+  timestamp?: Date;
 }
 
 const AttendanceSchema = new Schema<IAttendance>({
-  date: { type: Date, required: true },
+  date: { type: Date, required: true, index: true },
   class: { type: Schema.Types.ObjectId, ref: "Class", required: true },
   teacher: { type: Schema.Types.ObjectId, ref: "Teacher", required: true },
   student: { type: Schema.Types.ObjectId, ref: "Student", required: true },
+  isPresent: {
+    type: String,
+    required: true,
+    enum: ["present", "absent", "excused"],
+    default: "absent",
+  },
+  timestamp: { type: Date, index: true },
 });
 
 export const AttendanceModel = model<IAttendance>(
